a7b9e7c1617997401fb80ba8b53ab6f54effb4f7
AlexTighe <Alex@alextighe.co.uk>
2018-07-15 07:55:18 +0100

    More frames to scale & hide
    wow!!

--- DerpFrameScaler.lua
+++ DerpFrameScaler.lua
@@ -6,13 +6,15 @@ local scaler = 0.75
 LoadFrame:SetScript("OnEvent", function(self,event,...)
 	LoadAddOn("Blizzard_Communities") -- for BfA
 	LoadAddOn("Blizzard_AzeriteUI") -- for BfA
+	LoadAddOn("Blizzard_GarrisonUI")
 	LoadAddOn("Blizzard_WarboardUI")
 	LoadAddOn("Blizzard_AchievementUI")
 	LoadAddOn("Blizzard_TalentUI")
 	LoadAddOn("Blizzard_EncounterJournal")
 	LoadAddOn("Blizzard_Collections")
 	LoadAddOn("Blizzard_InspectUI")
-	--LoadAddOn("Blizzard_GuildUI") -- removed in BfA
+	--LoadAddOn("Blizzard_GuildUI") -- removed (void) in BfA
+	LoadAddOn("Blizzard_TradeSkillUI")
 	
 	-- Chat Widget Hiding
 	ChatFrameToggleVoiceDeafenButton:Hide() -- for BfA
@@ -25,6 +27,7 @@ LoadFrame:SetScript("OnEvent", function(self,event,...)
 	AzeriteEmpoweredItemUI:SetScale(scaler) -- for BfA
 	CommunitiesFrame:SetScale(scaler) -- for BfA
 	WarboardQuestChoiceFrame:SetScale(scaler) -- for BfA
+	GarrisonLandingPage:SetScale(scaler)
 	InspectFrame:SetScale(scaler)
 	GossipFrame:SetScale(scaler)
 	CollectionsJournal:SetScale(scaler)
@@ -37,12 +40,29 @@ LoadFrame:SetScript("OnEvent", function(self,event,...)
 	PlayerTalentFrame:SetScale(scaler)
 	QuestFrame:SetScale(scaler)
 	MerchantFrame:SetScale(scaler)
+	WardrobeFrame:SetScale(scaler)
+	TradeSkillFrame:SetScale(scaler)
 	--GuildFrame:SetScale(scaler) -- removed in BfA
 end)
 
--- Island Expiditions
+-- Island Expiditions UI scaling
 local IslandFrame = CreateFrame("Frame")
 IslandFrame:RegisterEvent("ISLANDS_QUEUE_OPEN")
 IslandFrame:SetScript("OnEvent",function(self,event,...)
 	IslandsQueueFrame:SetScale(scaler) 
 end)
+
+
+-- fuck off floating head
+local head = CreateFrame("Frame")
+function head:OnEvent(event, addon)
+	if addon == "Blizzard_TalkingHeadUI" then
+		hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
+				TalkingHeadFrame_CloseImmediately()
+		end)
+	self:UnregisterEvent(event)
+	end
+end
+head:RegisterEvent("ADDON_LOADED")
+head:SetScript("OnEvent", head.OnEvent)
+
