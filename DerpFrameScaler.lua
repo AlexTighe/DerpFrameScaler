435b30901d52bb60ddb9d29002ac97404e611caf
AlexTighe <Alex@alextighe.co.uk>
2018-07-20 12:16:25 +0100

    Making communities bareable for myself

--- DerpFrameScaler.lua
+++ DerpFrameScaler.lua
@@ -1,11 +1,11 @@
--- Lazy scaling of frames I want to do because I play at a stupid UIScale
+-- Some lazy frame scaling, and general UI Fixes that annoy me
+local scaler = 0.75 -- frame scale
 local LoadFrame = CreateFrame("Frame")
 LoadFrame:RegisterEvent("PLAYER_LOGIN")
-local scaler = 0.75
-
 LoadFrame:SetScript("OnEvent", function(self,event,...)
-	LoadAddOn("Blizzard_Communities") -- for BfA
-	LoadAddOn("Blizzard_AzeriteUI") -- for BfA
+	-- Loading addons so I can scale them on "login", dosen't seem to effect load time
+	LoadAddOn("Blizzard_Communities") 
+	LoadAddOn("Blizzard_AzeriteUI") 
 	LoadAddOn("Blizzard_GarrisonUI")
 	LoadAddOn("Blizzard_WarboardUI")
 	LoadAddOn("Blizzard_AchievementUI")
@@ -13,20 +13,21 @@ LoadFrame:SetScript("OnEvent", function(self,event,...)
 	LoadAddOn("Blizzard_EncounterJournal")
 	LoadAddOn("Blizzard_Collections")
 	LoadAddOn("Blizzard_InspectUI")
-	--LoadAddOn("Blizzard_GuildUI") -- removed (void) in BfA
+	LoadAddOn("Blizzard_GuildUI") -- So I can use old Guild UI, and use Communities as a "Chat History"
 	LoadAddOn("Blizzard_TradeSkillUI")
 	
-	-- Chat Widget Hiding
-	ChatFrameToggleVoiceDeafenButton:Hide() -- for BfA
-	ChatFrameToggleVoiceMuteButton:Hide() -- for BfA
+	-- Chat Widget hiding
+	ChatFrameToggleVoiceDeafenButton:ClearAllPoints()
+	ChatFrameToggleVoiceDeafenButton:Hide() 
+	ChatFrameToggleVoiceMuteButton:ClearAllPoints()
+	ChatFrameToggleVoiceMuteButton:Hide()
+	ChatFrameChannelButton:ClearAllPoints()
+	ChatFrameChannelButton:Hide()
 	
-	-- Exclusive for BfA Beta
-	Confused:GetParent():SetAlpha(0) -- for BfA Beta Feedback box
-	Confused:GetParent():SetScale(0.1) -- for BfA Beta Feedback box
-	
-	AzeriteEmpoweredItemUI:SetScale(scaler) -- for BfA
-	CommunitiesFrame:SetScale(scaler) -- for BfA
-	WarboardQuestChoiceFrame:SetScale(scaler) -- for BfA
+	-- Scaling Frames to non awful sizes
+	AzeriteEmpoweredItemUI:SetScale(scaler) 
+	CommunitiesFrame:SetScale(scaler) 
+	WarboardQuestChoiceFrame:SetScale(scaler) 
 	GarrisonLandingPage:SetScale(scaler)
 	InspectFrame:SetScale(scaler)
 	GossipFrame:SetScale(scaler)
@@ -42,18 +43,29 @@ LoadFrame:SetScript("OnEvent", function(self,event,...)
 	MerchantFrame:SetScale(scaler)
 	WardrobeFrame:SetScale(scaler)
 	TradeSkillFrame:SetScale(scaler)
-	--GuildFrame:SetScale(scaler) -- removed in BfA
+	GuildFrame:SetScale(scaler)
+	
+	-- Dirty little hook on ToggleGuildFrame
+	ToggleGuildFrame = function() 
+		if CommunitiesFrame:IsVisible() or GuildFrame:IsVisible() then
+			HideUIPanel(CommunitiesFrame)
+			HideUIPanel(GuildFrame)
+		else
+			ShowUIPanel(CommunitiesFrame)
+			ShowUIPanel(GuildFrame)
+			showRosterHack()
+		end
+	end
 end)
 
--- Island Expiditions UI scaling
+-- Island Expedition scaling
 local IslandFrame = CreateFrame("Frame")
 IslandFrame:RegisterEvent("ISLANDS_QUEUE_OPEN")
 IslandFrame:SetScript("OnEvent",function(self,event,...)
 	IslandsQueueFrame:SetScale(scaler) 
 end)
 
-
--- fuck off floating head
+-- Hiding Talking Heads, borrowed from another addon
 local head = CreateFrame("Frame")
 function head:OnEvent(event, addon)
 	if addon == "Blizzard_TalkingHeadUI" then
@@ -65,4 +77,21 @@ function head:OnEvent(event, addon)
 end
 head:RegisterEvent("ADDON_LOADED")
 head:SetScript("OnEvent", head.OnEvent)
+
+-- Manually setting Guildframe to show roster on show
+function showRosterHack()
+	ButtonFrameTemplate_HideButtonBar(GuildFrame);
+	GuildFrame_ShowPanel("GuildRosterFrame");
+	GuildFrameInset:SetPoint("TOPLEFT", 4, -90);
+	GuildFrameInset:SetPoint("BOTTOMRIGHT", -7, 26);
+	GuildFrameBottomInset:Hide();
+	GuildPointFrame:Hide();
+	GuildFactionFrame:Hide();
+	updateRosterCount = true;
+	GuildFrameMembersCountLabel:Show();
+	PanelTemplates_SetTab(GuildFrame, 2); 
+end
+
+
+
 
